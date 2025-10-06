719fb6e686b33ed0b4188712a902ce4f380e2c85
Tahsina2226 <tahsinatanvin274@gmail.com>
2025-10-06 22:26:56 +0600

    ready for deploy

--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,16 +6,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = handler;
 const app_1 = __importDefault(require("./app"));
 function handler(req, res) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield new Promise((resolve) => {
-                (0, app_1.default)(req, res);
-                resolve();
-            });
-        }
-        catch (err) {
-            console.error(err);
-            res.status(500).json({ message: "Internal Server Error" });
-        }
-    });
+    (0, app_1.default)(req, res);
 }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "./app";
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  try {
-    await new Promise<void>((resolve) => {
-      app(req as any, res as any);
-      resolve();
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  app(req as any, res as any);
 }
